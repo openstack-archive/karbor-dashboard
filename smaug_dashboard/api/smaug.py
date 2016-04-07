85cc4309ba837483ea68a628b46b28ca987c3aa2
xiangxinyong <xiangxinyong@huawei.com>
2016-04-07 20:31:27 +0800

    Submit Protection Plan API for dashboard
    Change-Id: Ib88ddf35c17848722751f8930825450361678aa5
    Closes-Bug: #1567397

--- smaug_dashboard/api/smaug.py
+++ smaug_dashboard/api/smaug.py
@@ -18,6 +18,7 @@ import six.moves.urllib.parse as urlparse
 
 from django.conf import settings
 from horizon import exceptions
+from horizon.utils import functions as utils
 from horizon.utils.memoized import memoized
 from openstack_dashboard.api import base
 from smaugclient.v1 import client as smaug_client
@@ -53,3 +54,84 @@ def smaugclient(request):
                             auth_plugin='token',
                             )
     return c
+
+
+def update_pagination(entities, page_size, marker, sort_dir, sort_key,
+                      reversed_order):
+    has_more_data = has_prev_data = False
+    if len(entities) > page_size:
+        has_more_data = True
+        entities.pop()
+        if marker is not None:
+            has_prev_data = True
+    # first page condition when reached via prev back
+    elif reversed_order and marker is not None:
+        has_more_data = True
+    # last page condition
+    elif marker is not None:
+        has_prev_data = True
+
+    # restore the original ordering here
+    if reversed_order:
+        entities = sorted(entities, key=lambda entity:
+                          (getattr(entity, sort_key) or '').lower(),
+                          reverse=(sort_dir == 'asc'))
+
+    return entities, has_more_data, has_prev_data
+
+
+def plan_create(request, name, provider_id, resources):
+    return smaugclient(request).plans.create(name, provider_id, resources)
+
+
+def plan_delete(request, plan_id):
+    return smaugclient(request).plans.delete(plan_id)
+
+
+def plan_update(request, plan_id, data):
+    return smaugclient(request).plans.update(plan_id, data)
+
+
+def plan_list(request, detailed=False, search_opts=None, marker=None,
+              limit=None, sort_key=None, sort_dir=None, sort=None):
+    return smaugclient(request).plans.list(detailed=detailed,
+                                           search_opts=search_opts,
+                                           marker=marker,
+                                           limit=limit,
+                                           sort_key=sort_key,
+                                           sort_dir=sort_dir,
+                                           sort=sort)
+
+
+def plan_list_paged(request, detailed=False, search_opts=None, marker=None,
+                    limit=None, sort_key=None, sort_dir=None, sort=None,
+                    paginate=False, reversed_order=False):
+    has_more_data = False
+    has_prev_data = False
+
+    if paginate:
+        if reversed_order:
+            sort_dir = 'desc' if sort_dir == 'asc' else 'asc'
+        page_size = utils.get_page_size(request)
+        plans = smaugclient(request).plans.list(detailed=detailed,
+                                                search_opts=search_opts,
+                                                marker=marker,
+                                                limit=page_size + 1,
+                                                sort_key=sort_key,
+                                                sort_dir=sort_dir,
+                                                sort=sort)
+        plans, has_more_data, has_prev_data = update_pagination(
+            plans, page_size, marker, sort_dir, sort_key, reversed_order)
+    else:
+        plans = smaugclient(request).plans.list(detailed=detailed,
+                                                search_opts=search_opts,
+                                                marker=marker,
+                                                limit=limit,
+                                                sort_key=sort_key,
+                                                sort_dir=sort_dir,
+                                                sort=sort)
+    return (plans, has_more_data, has_prev_data)
+
+
+def plan_get(request, plan_id):
+    return smaugclient(request).plans.get(plan_id)
