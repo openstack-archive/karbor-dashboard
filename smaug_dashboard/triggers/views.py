04a4b51828716d8789820df4423e98d3dc3fa983
xiangxinyong <xiangxinyong@huawei.com>
2016-04-10 12:22:19 +0800

    [Triggers] Submit index view function
    Change-Id: Ide6b58b6efa14cc1b04689a0f64507a2e7fb8963
    Closes-Bug: #1568442

--- smaug_dashboard/triggers/views.py
+++ smaug_dashboard/triggers/views.py
@@ -12,9 +12,49 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from horizon import views
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import exceptions
+from horizon import tables as horizon_tables
+
+from smaug_dashboard.api import smaug as smaugclient
+from smaug_dashboard.triggers import tables
 
 
-class IndexView(views.APIView):
-    # A very simple class-based view...
+class IndexView(horizon_tables.DataTableView):
+    table_class = tables.TriggersTable
     template_name = 'triggers/index.html'
+    page_title = _("Triggers")
+
+    def has_prev_data(self, table):
+        return self._prev
+
+    def has_more_data(self, table):
+        return self._more
+
+    def get_data(self):
+        request = self.request
+        prev_marker = request.GET.get(
+            tables.TriggersTable._meta.prev_pagination_param, None)
+
+        if prev_marker is not None:
+            marker = prev_marker
+        else:
+            marker = request.GET.get(
+                tables.TriggersTable._meta.pagination_param, None)
+        reversed_order = prev_marker is not None
+        triggers = []
+        try:
+            triggers, self._more, self._prev = smaugclient.trigger_list_paged(
+                request, None,
+                marker=marker,
+                paginate=True,
+                sort_dir='asc',
+                sort_key='name',
+                reversed_order=reversed_order)
+        except Exception:
+            self._prev = False
+            self._more = False
+            exceptions.handle(self.request,
+                              _('Unable to retrieve triggers list.'))
+        return triggers
