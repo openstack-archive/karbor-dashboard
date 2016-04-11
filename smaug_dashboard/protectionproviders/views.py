a5aad71c7359c9a78ceef570bcd4734617624c48
xiangxinyong <xiangxinyong@huawei.com>
2016-04-11 16:52:21 +0800

    [Protection Providers] Submit index view function
    Change-Id: I7e271fd41e0303e188bb615ca61d4520d3fc9759
    Closes-Bug: #1568745

--- smaug_dashboard/protectionproviders/views.py
+++ smaug_dashboard/protectionproviders/views.py
@@ -12,9 +12,51 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from horizon import views
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import exceptions
+from horizon import tables as horizon_tables
+
+from smaug_dashboard.api import smaug as smaugclient
+from smaug_dashboard.protectionproviders import tables
 
 
-class IndexView(views.APIView):
-    # A very simple class-based view...
+class IndexView(horizon_tables.DataTableView):
+    table_class = tables.ProtectionProvidersTable
     template_name = 'protectionproviders/index.html'
+    page_title = _("Protection Providers")
+
+    def has_prev_data(self, table):
+        return self._prev
+
+    def has_more_data(self, table):
+        return self._more
+
+    def get_data(self):
+        request = self.request
+        prev_marker = request.GET.get(
+            tables.ProtectionProvidersTable._meta.prev_pagination_param, None)
+
+        if prev_marker is not None:
+            marker = prev_marker
+        else:
+            marker = request.GET.get(
+                tables.ProtectionProvidersTable._meta.pagination_param, None)
+            reversed_order = prev_marker is not None
+        providers = []
+        try:
+            providers, self._more, self._prev = \
+                smaugclient.provider_list_paged(
+                    request, None,
+                    marker=marker,
+                    paginate=True,
+                    sort_dir='asc',
+                    sort_key='name',
+                    reversed_order=reversed_order)
+        except Exception:
+            self._prev = False
+            self._more = False
+            exceptions.handle(
+                self.request,
+                _('Unable to retrieve protection providers list.'))
+        return providers
